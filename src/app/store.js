0f7dc2a81ac76c83a9ca6635a7343c8972f5147d
“jerumanu” <emanueljeru200@gmail.com>
2022-07-16 12:03:20 +0300

    create configuringStore and add reducers

---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import postReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
+
+
+export const store = configureStore({
+    reducer :{
+        posts:postReducer,
+        users:usersReducer
+
+    }
+
+})
